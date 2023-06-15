0595d3712346e221b81008af1c50ef9b1c80fb64
dgress95 <108832982+dgress95@users.noreply.github.com>
2023-06-14 20:45:30 -0400

    Styling UI

--- solver.js
+++ solver.js
@@ -52,6 +52,34 @@ function search() {
  
 }
 
+function useModal() {
+    // Get the modal
+    let modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    let btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    let span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+         modal.style.display = "none";
+}
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -61,5 +89,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const btn = document.getElementById('search');
     current.addEventListener("keyup", populateAnswer);
     btn.addEventListener('click', search);
+    useModal();
 
 });
