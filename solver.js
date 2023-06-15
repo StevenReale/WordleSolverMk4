b5ce28eb7d43b447241ed999e5413ab36c2b0d9e
Steven Reale <stevenmreale@gmail.com>
2023-06-15 05:24:47 -0400

    identifying known letters in progress

--- solver.js
+++ solver.js
@@ -1,4 +1,5 @@
 let currentAnswerState;
+let knownLetterBlanks = 0;
 
 
 
@@ -24,7 +25,17 @@ function populateAnswer() {
 }
 
 function search() {
+    //if (currentAnswerState == undefined) return;
     let possibleAnswers = [];
+    const knownLetterDivs = document.querySelectorAll('.known');
+    const knowns = {};
+
+    for (thisDiv of knownLetterDivs) {
+        const divId = (Number.parseInt(thisDiv.getAttribute('id').substring(3)));
+        const checkboxes = document.querySelectorAll("#letter-not-in" + divId);
+        console.log(checkboxes);
+    }
+
     for (let word of wordlist) {
         let found = true;
         for (let i = 0; i < 5; i++) {
@@ -53,14 +64,67 @@ function search() {
  
 }
 
+function addKnownLetterBlank() {
+    knownLetterBlanks++;
+
+    //create new div to house new blanks
+    const blankSectionButton = document.getElementById('new-known-letter-blank');
+    const letterDiv = document.createElement('div');
+    letterDiv.id = "div" + knownLetterBlanks;
+    letterDiv.classList.add('known');
+    blankSectionButton.insertAdjacentElement("beforebegin", letterDiv);
+
+    //create label for letter input
+    const letterLabel = document.createElement('label');
+    const label = "known-letter-blank-" + knownLetterBlanks;
+    letterLabel.setAttribute("for", label);
+    letterLabel.innerText = "Input next known letter: ";
+    letterDiv.appendChild(letterLabel);
+
+    //create letter input
+    const letter = document.createElement('input');
+    letter.classList.add('known-letter');
+    letter.name = label;
+    letter.id = label;
+    letter.type = 'text';
+    letterDiv.appendChild(letter);
+
+    //create label for blanks input
+    const blanksLabel = document.createElement('label');
+    const labelForBlank = "letter-not-in-" + knownLetterBlanks;
+    blanksLabel.setAttribute("for", labelForBlank);
+    blanksLabel.innerText = 'Select blanks where it is known that this letter does not appear: ';
+    letterDiv.appendChild(blanksLabel);
+
+    //create blanks input
+    for (let i = 0; i < 5; i++) {
+        const check = document.createElement('input');
+        check.type = 'checkbox';
+        check.id = labelForBlank;
+        check.name = i;
+        letterDiv.appendChild(check);
+    }
+
+    //create delete button
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = '-';
+    letterDiv.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', () => letterDiv.remove());
+    
+    
+
+
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // wordList.push([...readfile("data/wordle-LA.txt")]);
-    // console.log(wordList);
     const current = document.getElementById('current');
-    const btn = document.getElementById('search');
+    const searchBtn = document.getElementById('search');
+    const blanksBtn = document.getElementById('new-known-letter-blank');
     current.addEventListener("keyup", populateAnswer);
-    btn.addEventListener('click', search);
+    searchBtn.addEventListener('click', search);
+    blanksBtn.addEventListener('click', addKnownLetterBlank);
+    addKnownLetterBlank();
 
 });
